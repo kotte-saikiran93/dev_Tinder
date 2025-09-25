85e22573dbfbf13177ce35920c4e26d750a8f925
kotte-saikiran93 <saikirankotte003@gmail.com>
2025-09-25 17:56:52 +0530

    exploring get, post,delete

--- src/app.js
+++ src/app.js
@@ -2,19 +2,44 @@ const server=require("express");
 const app=server();
 
 
+app.get("/user",(req,res)=>{
+    res.send({firstname:
+        "saikiran",
+        lastname:"kotte"
+    });
+});
+
+
+app.post("/user",(req,res)=>{
+    res.send("data posted successfully");
+});
+
+
+app.delete("/user",(req,res)=>{
+    res.send("data deleted successfully");
+});
+
+
+// app.use("/hello",(req,res)=>{
+//     res.send("hello");
+// });
+
 
 // app.use((req,res)=>{
 //     res.send("helloworld this is you");
 // });
-app.use("/",(req,res)=>{
-    res.send("saikiran hello how are");
-});
-app.use("/test",(req,res)=>{
-    res.send("test");
-});
-app.use("/hello",(req,res)=>{
-    res.send("hello");
-});
+
+
+// app.use("/",(req,res)=>{
+//     res.send("saikiran hello how ");
+// });
+
+
+// app.use("/test",(req,res)=>{
+
+//     res.send("test");
+// });
+
 
 app.listen(7777, ()=>{
     console.log("hii saikiran")
